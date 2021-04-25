ec3a5eb15c9af02b9851a53b8706641f401cd8cb
Michael Dadi <mdadi1130@users.noreply.github.com>
2021-04-25 13:32:15 -0400

    Create CircularQueue.py
    Circular queue for sender/receiver data buffers

---
+++ CircularQueue.py
@@ -0,0 +1,26 @@
+class CircularQueue:
+    def __init__(self, k):
+        self.k = k
+        self.queue = [None] * k
+        self.head = self.tail = -1
+
+    # Insert operation
+    def enqueue(self, data):
+        if self.head == -1:
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
+        else:
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+
+    def dequeue(self):
+        if self.head == self.tail:
+            temp = self.queue[self.head]
+            self.head = -1
+            self.tail = -1
+            return temp
+        else:
+            temp = self.queue[self.head]
+            self.head = (self.head + 1) % self.k
+            return temp
